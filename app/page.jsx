f86e2f1fd8526627fb82f3aca4b36c19301a36fe
apsamade <bouderga.abdessamade@gmail.com>
2024-05-16 16:01:38 +0200

    maj

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,7 @@ const Home = () => {
                 <BackgroundAnime />
                 <div className="xl:max-w-[800px] p-4 mx-auto max-w-[600px] xl:ml-32 xl:mt-24">
                     <span className="uppercase pb-4 block text-primary">BTP Platform</span>
-                    <h1 className="xl:text-6xl text-nowrap text-3xl uppercase font-semibold">Premier Titre <strong className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-teal-700">Principale</strong></h1>
+                    <h1 className="xl:text-6xl xl:text-nowrap text-3xl uppercase font-semibold">Premier Titre <strong className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-teal-700">Principale</strong></h1>
                     <hr className="max-w-[150px] border-white mb-4 mt-2" />
                     <p className="leading-6 font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, delectus sint fugiat ut animi nemo reiciendis voluptates architecto sed accusamus eaque expedita ipsa earum, unde, placeat non odio? Unde, dicta.</p>
                     <div className="flex my-5 items-center justify-center flex-wrap">
@@ -49,7 +49,7 @@ const Home = () => {
             <section className="bg-white">
                 <div className="max-w-[600px] mx-auto mt-24 pb-12">
                     <strong className="bg-clip-text tracking-[2px] uppercase text-center mx-auto block py-2 text-transparent bg-gradient-to-r from-primary to-teal-700">Services</strong>
-                    <h2 className="font-bold text-teal-950 text-center mx-auto text-5xl">Deuxieme titre du site pour les services</h2>
+                    <h2 className="font-bold text-teal-950 text-center mx-auto text-3xl xl:text-5xl">Deuxieme titre du site pour les services</h2>
                 </div>
                 <section className="flex items-center justify-center mx-auto flex-wrap">
                     <Image
@@ -57,7 +57,7 @@ const Home = () => {
                         src="/assets/elements/building.png"
                         width={450}
                         height={450}
-                        className="pb-16"
+                        className="xl:pb-16 px-4 xl:static relative bottom-16"
                     />
                     <div className="xl:pl-14 px-8 xl:pt-12 pb-0">
                         <strong className="bg-clip-text tracking-[2px] py-3 block text-transparent bg-gradient-to-r uppercase from-teal-400 to-teal-700">Simple features</strong>
@@ -74,7 +74,7 @@ const Home = () => {
                         <div className="flex my-4 p-2 items-center justify-start">
                             <IoCheckmark className="text-teal-800 p-2 bg-green-200 rounded-[100%] text-4xl mr-2" /> <p>Une information quelconque <strong>Élément</strong></p>
                         </div>
-                        <Link href='/' className="p-3 mt-12 block w-fit py-4 text-white px-24 hover:px-32 duration-200 hover:bg-teal-500 shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-primary">
+                        <Link href='/' className="p-3 w-full text-center mt-8 block xl:w-fit py-4 text-white px-24 hover:px-32 duration-200 hover:bg-teal-500 shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-primary">
                             Contact
                         </Link>
                     </div>
@@ -84,9 +84,9 @@ const Home = () => {
                     src="/assets/elements/lien.png"
                     width={600}
                     height={450}
-                    className="mx-auto block"
+                    className="mx-auto xl:block hidden"
                 />
-                <section className="flex items-center justify-center mx-auto flex-wrap">
+                <section className="flex items-center xl:flex-row flex-col-reverse justify-center mx-auto flex-wrap xl:mt-0 mt-24">
                     <div className="xl:pl-14 px-8 xl:pt-0 pb-32">
                         <strong className="bg-clip-text tracking-[2px] py-3 block text-transparent bg-gradient-to-r uppercase from-teal-400 to-teal-700">Simple features</strong>
                         <h3 className="text-3xl font-semibold">Pour les ouvriers.</h3>
@@ -102,7 +102,7 @@ const Home = () => {
                         <div className="flex my-4 pl-0 p-2 items-center justify-start">
                             <IoCheckmark className="text-teal-800 p-2 bg-green-200 rounded-[100%] text-4xl mr-2" /> <p>Une information quelconque <strong>Élément</strong></p>
                         </div>
-                        <Link href='/' className="p-3 mt-12 block w-fit py-4 text-white px-24 hover:px-32 duration-200 hover:bg-teal-500 shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-primary">
+                        <Link href='/' className="p-3 w-full text-center mt-8 block xl:w-fit py-4 text-white px-24 hover:px-32 duration-200 hover:bg-teal-500 shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-primary">
                             Contact
                         </Link>
                     </div>
@@ -111,23 +111,23 @@ const Home = () => {
                         src="/assets/elements/building.png"
                         width={500}
                         height={450}
-                        className="pb-16 relative bottom-24"
+                        className="xl:pb-16 pb-8 xl:px-0 px-4 xl:relative bottom-24"
                     />
                 </section>
-                <section className="max-w-[1000px] mb-24 overflow-hidden z-0 relative text-white mx-auto flex items-center justify-around p-4 rounded-lg bg-gradient-to-l to-teal-800 from-primary">
+                <section className="max-w-[1000px] px-4 xl:px-12 p-12 mx-4 mb-24 overflow-hidden z-0 relative text-white md:mx-auto flex items-center justify-around rounded-lg bg-gradient-to-l to-teal-800 from-primary">
                     <BackgroundAnime />
                     <Image
                         alt="développemnt dans le BTP"
                         src="/assets/elements/woman_3d.png"
                         width={300}
                         height={210}
-                        className="px-12"
+                        className="px-12 hidden xl:block"
                     />
                     <div>
                         <h2 className="text-3xl font-bold pb-4">Troisieme titre du site internet</h2>
                         <p className="leading-6 my-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est fuga, explicabo, nisi quas unde, consequatur illo odit earum fugit non corrupti iste esse veniam atque! Repudiandae laboriosam autem blanditiis repellendus?</p>
                         <p className="leading-6 mt-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est fuga, explicabo, nisi quas unde, consequatur illo odit earum fugit non corrupti iste esse veniam atque! Repudiandae laboriosam autem blanditiis repellendus?</p>
-                        <Link href='/' className="p-3 mt-8 block w-fit py-4 text-primary px-24 hover:px-32 duration-200 hover:bg-black shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-white">
+                        <Link href='/' className="p-3 w-full text-center mt-8 block xl:w-fit py-4 text-primary px-24 hover:px-32 duration-200 hover:bg-black shadow-2xl font-semibold ml-0 m-2 rounded-3xl bg-white">
                             Contact
                         </Link>
                     </div>
